b17c42e75dd47605a8490d7f83026ebfb717235d
Highkam <kamilonn2004@hotmail.com>
2025-09-19 22:52:54 -0500

    Added init structures and services

---
+++ services/cart-service/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cart Service')
+    .setDescription('Contratos iniciales del microservicio de carrito de compras')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3002);
+}
+bootstrap();
